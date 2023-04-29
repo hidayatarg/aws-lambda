184b9b8f24b9881f6641ab83155ec5d1572d9c82
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-04-30 00:14:24 +0300

    inital

--- lib/lambda-cdk-stack.ts
+++ lib/lambda-cdk-stack.ts
@@ -1,16 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 
 export class LambdaCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // defines an AWS Lambda resource
+    const hello = new Function(this, 'HelloHandler', {
+      runtime: Runtime.NODEJS_14_X,    // execution environment
+      code: Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new LambdaRestApi(this, 'ApiGateway', {
+      handler: hello
+    });
   }
 }
